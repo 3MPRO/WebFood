82d4b004a313989d3a40acdd2c70790c32a8d45c
Phạm Văn Thiên <80672239+VanThienPham@users.noreply.github.com>
2021-11-25 19:48:48 +0700

    edit sanpham

--- Views/infoedit.php
+++ Views/infoedit.php
@@ -25,46 +25,72 @@
                     <form  action="?act=taikhoan&xuli=updateinfo" method="POST">
                         <label class="container-infor__title" >Thông Tin Tài Khoản</label>
                         <h2 class ="container-infor__name">Khách hàng: <span><?= $data['Ho']. " ".$data['Ten'] ?></span></h2>
-                        <fieldset class="container-infor__item name-login">
-                            <label>Tên Đăng nhập</label>
-                            <span class="required">*</span>
-                            <input name = "TaiKhoan" type="text" value="<?= $data['TaiKhoan']?>" class="form-control form-control-lg edit  " placeholder=""/>
-                            <div class="search-result"></div>
-                        </fieldset>
-                    
-                               
-                        </fieldset>
-                        <fieldset class="container-infor__item">
-                            <label>Họ </label>
-                            <span class="required">*</span>
-                            <input type="text" name ="Ho" value ="<?= $data['Ho'] ?>"class="form-control form-control-lg edit" placeholder=""/>
-                        </fieldset>
-                        <fieldset class="container-infor__item">
-                            <label>Tên </label>
-                            <span class="required">*</span>
-                            <input type="text" name ="Ten" value ="<?= $data['Ten'] ?>"class="form-control form-control-lg edit" placeholder=""/>
-                        </fieldset>
-    
-                        <fieldset class="container-infor__item">
-                            <label>Số điện thoại </label>
-                            <span class="required">*</span>
-                            <input type="tel" name ="SDT" value="<?= $data['SDT']?>" class="form-control form-control-lg edit" placeholder=""/>
-                        </fieldset>
-                        <fieldset class="container-infor__item">
-                            <label>Email </label>
-                            <span class="required">*</span>
-                            <input type="email" name ="Email" value="<?= $data['Email']?>" class="form-control form-control-lg edit" placeholder=""/>
-                        </fieldset>
-                        <fieldset class="container-infor__item">
-                            <label>Địa chỉ </label>
-                            <span class="required">*</span>
-                            <input type="text" name = "DiaChi" value="<?= $data['DiaChi']?>" class="form-control form-control-lg edit" placeholder=""/>
-                        </fieldset>
-                        <a href="?act=taikhoan&xuli=updateinfo">
+                        <span><input class="infoChange-type" type="checkbox" name="" id="" > Thông tin cá nhân </span>
+                        <div class="container-inforChange">
+                            <fieldset class="container-infor__item name-login">
+                                <label>Tên Đăng nhập</label>
+                                <span class="required">*</span>
+                                <input name = "TaiKhoan" type="text" value="<?= $data['TaiKhoan']?>" class="form-control form-control-lg edit  " placeholder=""/>
+                                <div class="search-result"></div>
+                            </fieldset>
+                        
+                                
+                            </fieldset>
+                            <fieldset class="container-infor__item">
+                                <label>Họ </label>
+                                <span class="required">*</span>
+                                <input type="text" name ="Ho" value ="<?= $data['Ho'] ?>"class="form-control form-control-lg edit" placeholder=""/>
+                            </fieldset>
+                            <fieldset class="container-infor__item">
+                                <label>Tên </label>
+                                <span class="required">*</span>
+                                <input type="text" name ="Ten" value ="<?= $data['Ten'] ?>"class="form-control form-control-lg edit" placeholder=""/>
+                            </fieldset>
+        
+                            <fieldset class="container-infor__item">
+                                <label>Số điện thoại </label>
+                                <span class="required">*</span>
+                                <input type="tel" name ="SDT" value="<?= $data['SDT']?>" class="form-control form-control-lg edit" placeholder=""/>
+                            </fieldset>
+                            <fieldset class="container-infor__item">
+                                <label>Email </label>
+                                <span class="required">*</span>
+                                <input type="email" name ="Email" value="<?= $data['Email']?>" class="form-control form-control-lg edit" placeholder=""/>
+                            </fieldset>
+                            <fieldset class="container-infor__item">
+                                <label>Địa chỉ </label>
+                                <span class="required">*</span>
+                                <input type="text" name = "DiaChi" value="<?= $data['DiaChi']?>" class="form-control form-control-lg edit" placeholder=""/>
+                            </fieldset>
+                        </div>
+                        
+                        <!-- Đổi mật khẩu -->
+                        <span><input class= "infoChange-type"type="checkbox" name="" id="" > Đổi mật khẩu </span>
+                        <div class="container-inforChange">
+                                 <fieldset class="container-infor__item">
+                                    <label>Mật khẩu củ</label>
+                                  
+                                    <input type="password" name="MatKhau" class="form-control form-control-lg edit" placeholder="Password"/>
+                                </fieldset>
+                                <fieldset class="container-infor__item">
+                                    <label>Mật khẩu mới</label>
+                                   
+                                    <input type="password" name="MatKhau" class="form-control form-control-lg edit" placeholder="Password"/>
+                                </fieldset>
+                                <fieldset class="container-infor__item">
+                                    <label>Nhập lại mật khẩu</label>
+                                    
+                                    <input type="password" name="check_password" class="form-control form-control-lg edit" placeholder="Password"/>
+                                </fieldset>
+                        </div>
+
+                       
                         <div class="container-infor__item">
                             <input class="btn-edit" type="submit" value="Cập Nhật">
                         </div>
-                         </a>
+                         
+                        
+                        
                     </form>
                   <?php } 
                   else {
