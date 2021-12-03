7ca8856e93d7283c0e640d11245e7ec0ef9df974
huykt1111 <huylmht10@gmail.com>
2021-12-03 10:18:26 +0700

    a

--- Admin/Views/user/list.php
+++ Admin/Views/user/list.php
@@ -44,10 +44,10 @@
         </td>
         <td>
           <a href="?mod=nguoidung&act=detail&id=<?= $row['MaND'] ?>" type="button" class="btn btn-success">Xem</a>
-          <?php //if (isset($_SESSION['isLogin_Admin']) && $_SESSION['isLogin_Admin'] == true) { ?>
+          <?php if (isset($_SESSION['isLogin_Admin']) && $_SESSION['isLogin_Admin'] == true) { ?>
           <a href="?mod=nguoidung&act=edit&id=<?= $row['MaND'] ?>" type="button" class="btn btn-warning">Sửa</a>
           <a href="?mod=nguoidung&act=delete&id=<?= $row['MaND'] ?>" onclick="return confirm('Bạn có thật sự muốn xóa ?');" type="button" class="btn btn-danger">Xóa</a>
-          <?php //}?>
+          <?php }?>
         </td>
       </tr>
     <?php } ?>
